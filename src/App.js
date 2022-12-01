25db6f6011a0f767fd86af77a08bf29632ad3411
Vaibhav Sahare <vaibhavsahare97@gmail.com>
2022-12-02 02:42:02 +0530

    Added Bookmark functionality, remove unwanted code(clean code)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Images from './components/Images';
-import Mainpage from './components/Mainpage';
 
 function App() {
   return (
     <div className="App">
-      <Mainpage />
       <Images />
     </div>
   );
--- src/components/Images.js
+++ src/components/Images.js
@@ -7,35 +7,35 @@ const Images = () => {
 
     const [photos, setPhotos] = useState([]);
     const [search, setSearch] = useState("");
-    // const [description, setDescription] = useState([]);
     const [bookmarks, setBookmarks] = useState([]);
+    const [isClicked, setIsClicked] = useState(false);
 
     const fetchPhotos = async (e) => {
         e.preventDefault();
-
         unsplash.search.getPhotos({
             query: search
         }).then(data => { setPhotos(data.response.results) })
-
     }
-
-    // useEffect(() => {
-    //     fetchPhotos()
-    // }, [])
 
     const changeHandler = (e) => {
         setSearch(e.target.value);
-        // console.log(search);
+    }
+
+    const handleBookmark = () => {
+        setIsClicked(!isClicked);
     }
 
     const bookmarksHandler = (e) => {
-        // console.log(e.target.parentNode);
-        setBookmarks([...bookmarks, e.target.parentNode]);
-        console.log(bookmarks);
+        setBookmarks([...bookmarks, e.target.parentNode.firstChild.src]);
     }
 
     return (
         <>
+            <div className='top'>
+                <h1>React Photo Search</h1>
+                <button className='bookmarks' onClick={handleBookmark}>Bookmarks</button>
+            </div>
+
             <form onSubmit={fetchPhotos}>
                 <input
                     type="text"
@@ -48,21 +48,40 @@ const Images = () => {
             </form>
 
             <div className='image-container'>
-                {
-                    photos.map((image) => {
-                        return (
-                            <div className="imageDiv" key={image.id}>
-                                <img
-                                    className='mainImage'
-                                    src={image.urls.small}
-                                    // alt={(!image.alt_description) ? (image.alt_description) : (image.sponsorship.tagline)}
-                                    alt="imageError"
-                                />
-                                <div className='add remove' onClick={bookmarksHandler}>ADD TO BOOKMARK</div>
-                            </div>
-                        )
-                    })
-                }
+
+                {isClicked ?
+                    (
+                        <>
+                            <p className='bookHeading'>BOOKMARKS ⇓ ⇒</p>
+                            {bookmarks.map((image, i) => {
+                                return (
+                                    <div className="imageDiv" key={i}>
+                                        <img
+                                            className='mainImage'
+                                            src={image}
+                                            alt="imageError"
+                                        />
+                                    </div>
+                                )
+                            })}
+                        </>
+                    )
+                    :
+                    (
+                        photos.map((image) => {
+                            return (
+                                <div className="imageDiv" key={image.id}>
+                                    <img
+                                        className='mainImage'
+                                        src={image.urls.small}
+                                        alt="imageError"
+                                    />
+                                    <div className='add remove' onClick={bookmarksHandler}>ADD TO BOOKMARK</div>
+                                </div>
+                            )
+                        })
+                    )}
+
             </div>
         </>
     )
